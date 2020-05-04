7eeebbf0695e184683d3d49b2ec2493bc0dd89e7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-04 12:55:54 +0600

    full screen Image

--- app/src/main/java/com/luminous/android/admobbasic/ui/home/HomeFragment.java
+++ app/src/main/java/com/luminous/android/admobbasic/ui/home/HomeFragment.java
@@ -24,7 +24,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-       
+
         return root;
     }
 }
