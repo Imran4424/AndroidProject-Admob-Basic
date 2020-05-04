6a2a0ece12dbf36beeb373feacdd9a15334551c9
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-04 10:32:18 +0600

    full screen Image

---
+++ app/src/main/java/com/luminous/android/admobbasic/FullScreenImage.java
@@ -0,0 +1,14 @@
+package com.luminous.android.admobbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullScreenImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_image);
+    }
+}
