28e3e76b45831db46604fc3384d43d494379a5b5
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-04 14:31:11 +0600

    reward

--- app/src/main/java/com/luminous/android/admobbasic/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/luminous/android/admobbasic/ui/notifications/NotificationsFragment.java
@@ -1,22 +1,22 @@
 package com.luminous.android.admobbasic.ui.notifications;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.luminous.android.admobbasic.R;
 
 public class NotificationsFragment extends Fragment {
-
+    private MediaPlayer golpoPlayer;
+    private ImageView playImage;
     private NotificationsViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,6 +25,36 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
 
+        playImage = (ImageView) root.findViewById(R.id.playImage);
+        golpoPlayer = MediaPlayer.create(getActivity(), R.raw.sheyal_sharosh);
+
+
         return root;
+    }
+
+
+    public void play(View view) {
+        if(!golpoPlayer.isPlaying()) {
+             golpoPlayer.start();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        golpoPlayer.stop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        golpoPlayer.start();
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment childFragment) {
+        super.onAttachFragment(childFragment);
     }
 }
