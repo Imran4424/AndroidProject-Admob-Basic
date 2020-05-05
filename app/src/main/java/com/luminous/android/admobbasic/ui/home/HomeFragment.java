1e67f0e7c9931e81fc889b844ab4d87b813dbbcf
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-05 18:28:58 +0600

    set banner ads

--- app/src/main/java/com/luminous/android/admobbasic/ui/home/HomeFragment.java
+++ app/src/main/java/com/luminous/android/admobbasic/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.luminous.android.admobbasic.R;
@@ -30,6 +32,8 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        LinearLayout storyLinearLayout = root.findViewById(R.id.storyLinearLayout);
+
         storyText = root.findViewById(R.id.storyText);
 
         storyText.setText(R.string.BhooterSanarHat);
@@ -37,8 +41,10 @@ public class HomeFragment extends Fragment {
         adView = new AdView(getActivity());
         adView.setAdSize(AdSize.BANNER);
         adView.setAdUnitId("ca-app-pub-8350504222422488/9359151241");
+        storyLinearLayout.addView(adView);
 
-
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
         return root;
     }
