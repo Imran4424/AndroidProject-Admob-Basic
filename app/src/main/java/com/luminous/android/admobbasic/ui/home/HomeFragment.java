0cadbf9e7df3574385e1767fab69f5c21f7da822
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-12 00:11:37 +0600

    home fragment

--- app/src/main/java/com/luminous/android/admobbasic/ui/home/HomeFragment.java
+++ app/src/main/java/com/luminous/android/admobbasic/ui/home/HomeFragment.java
@@ -35,10 +35,9 @@ public class HomeFragment extends Fragment {
         storyText = root.findViewById(R.id.storyText);
 
         storyText.setText(R.string.BhooterSanarHat);
+
+
         
-
-
-
         AdView adView = root.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
